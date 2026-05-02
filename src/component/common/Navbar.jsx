91f97a5ab9c16162aeb1b5ac56dd2d922c66e97e
Sagor8187 <sdsagor8187@gmail.com>
2026-05-02 22:06:22 +0600

    navbar design complete

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -25,52 +25,32 @@ export default function Navbar() {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+            <ul  className="menu menu-horizontal px-1 font-bold">
+            <li>
+              <a>Home</a>
+            </li>
+            <li><a >All Tiles</a></li>
+            <li>
+              <a>My profile</a>
+            </li>
+          </ul>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="text-xl font-bold"><span className="text-2xl text-purple-700 ">Elite</span> Tiles</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul  className="menu menu-horizontal px-1 font-bold">
             <li>
-              <a>Item 1</a>
+              <a>Home</a>
             </li>
+            <li><a >All Tiles</a></li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
+              <a>My profile</a>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn bg-purple-600 text-white font-bold">Login</a>
         </div>
       </div>
     </div>
